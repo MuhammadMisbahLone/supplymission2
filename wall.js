a5417af814ff41da2c9e8eae155bd7e1755cd176
MuhammadMisbahLone <74707768+MuhammadMisbahLone@users.noreply.github.com>
2020-12-21 10:48:32 +0530

    Add files via upload

---
+++ wall.js
@@ -0,0 +1,17 @@
+class Wall{
+    constructor(x,y,width,height) {
+    var boptions = {restitution:0.8,friction:1.5,density:0.9};
+    this.body = Bodies.rectangle(x,y,width,height,boptions);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+    }
+    display() {
+    push();
+    translate(this.body.position.x,this.body.bopsition.y);
+    rotate(this.body.angle);
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height)
+    pop();
+    }
+    }
